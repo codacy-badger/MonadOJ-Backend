9614fdd94e3605c49894f7684db63ebc00efe7e0
YanWQ-monad <YanWQmonad@gmail.com>
2018-10-05 10:08:02 +0800

    Update web framework

--- web/middleware.py
+++ web/middleware.py
@@ -16,8 +16,18 @@ async def response(request, handler):
     Returns:
         aiohttp.web.Response: The response generate from the return value of handler
     """
+    def is_status_code(code):
+        return isinstance(code, int) and 100 <= code < 600
+
     logging.debug('Response handler...')
     r = await handler(request)
+    status = 200
+
+    if isinstance(r, tuple) and len(r) == 2:
+        _status, body = r
+        if is_status_code(_status):
+            r = body
+            status = _status
 
     # StreamResponse object
     if isinstance(r, web.StreamResponse):
@@ -33,40 +43,26 @@ async def response(request, handler):
     # body only
     if isinstance(r, str):
         if r.startswith('redirect:'):
-            # log_redirection(request.path, r[9:], request.__user__)
             return web.HTTPFound(r[9:])
-        resp = web.Response(body=r.encode('utf-8'))
+        resp = web.Response(body=r.encode('utf-8'), status=status)
         resp.content_type = 'text/html;charset=utf-8'
         return resp
 
     # json data
     if isinstance(r, dict):
-        if r.get('error', None) is None:
-            r['error'] = None
+        r['error'] = r.get('error', None)
         resp = web.Response(
-            body=json.dumps(r, ensure_ascii=False, default=lambda o: o.__dict__).encode('utf-8'))
+            body=json.dumps(r, ensure_ascii=False, default=lambda o: o.__dict__).encode('utf-8'),
+            status=status)
         resp.content_type = 'application/json;charset=utf-8'
         return resp
 
     # status code only
-    if isinstance(r, int) and r >= 100 and r < 600:
+    if is_status_code(r):
         return web.Response(status=r)
 
-    # status code and json data
-    if isinstance(r, tuple) and len(r) == 2:
-        t, m = r
-        if isinstance(t, int) and t >= 100 and t < 600:
-            if isinstance(m, dict):
-                if m.get('error', None) is None:
-                    m['error'] = None
-                resp = web.Response(status=t,
-                                    body=json.dumps(m, ensure_ascii=False, default=lambda o: o.__dict__).encode(
-                                        'utf-8'))
-                resp.content_type = 'application/json;charset=utf-8'
-                return resp
-            return web.Response(status=t, text=str(m))
     # default:
-    resp = web.Response(body=str(r).encode('utf-8'))
+    resp = web.Response(body=str(r).encode('utf-8'), status=status)
     resp.content_type = 'text/plain;charset=utf-8'
     return resp
 
